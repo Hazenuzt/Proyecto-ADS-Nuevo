d8dfa9fee2f325a580868f9f4d940bd320d097e1
Rodrixx02 <floresh780@gmail.com>
2025-05-30 13:04:00 -0600

    editar, agregar usuario admin

--- Distribuidora de carros/Administrador.Designer.cs
+++ Distribuidora de carros/Administrador.Designer.cs
@@ -34,18 +34,48 @@
             this.btnSalirB = new System.Windows.Forms.Button();
             this.btnEliminar = new System.Windows.Forms.Button();
             this.label11 = new System.Windows.Forms.Label();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.txtKilometraje = new System.Windows.Forms.TextBox();
+            this.txtMotor = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.cbIdVehiculo = new System.Windows.Forms.ComboBox();
+            this.cbTransmision = new System.Windows.Forms.ComboBox();
             this.tabControl1.SuspendLayout();
             this.tabPage3.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabControl1.Location = new System.Drawing.Point(1, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(452, 436);
+            this.tabControl1.Size = new System.Drawing.Size(564, 436);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage3
@@ -57,7 +87,7 @@
             this.tabPage3.Controls.Add(this.label11);
             this.tabPage3.Location = new System.Drawing.Point(4, 26);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(444, 406);
+            this.tabPage3.Size = new System.Drawing.Size(556, 406);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Borrar";
             // 
@@ -108,12 +138,264 @@
             this.label11.TabIndex = 0;
             this.label11.Text = "Selecciones una opción a borrar:";
             // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.White;
+            this.tabPage1.Controls.Add(this.cbTransmision);
+            this.tabPage1.Controls.Add(this.cbIdVehiculo);
+            this.tabPage1.Controls.Add(this.txtDescripcion);
+            this.tabPage1.Controls.Add(this.label12);
+            this.tabPage1.Controls.Add(this.txtColor);
+            this.tabPage1.Controls.Add(this.label10);
+            this.tabPage1.Controls.Add(this.label9);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.btnEditar);
+            this.tabPage1.Controls.Add(this.txtKilometraje);
+            this.tabPage1.Controls.Add(this.txtMotor);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Location = new System.Drawing.Point(4, 26);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(556, 406);
+            this.tabPage1.TabIndex = 3;
+            this.tabPage1.Text = "Editar";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.White;
+            this.tabPage2.Controls.Add(this.btnSalir);
+            this.tabPage2.Controls.Add(this.btnAgregar);
+            this.tabPage2.Controls.Add(this.txtPassword);
+            this.tabPage2.Controls.Add(this.txtUsername);
+            this.tabPage2.Controls.Add(this.txtNombre);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 26);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(556, 406);
+            this.tabPage2.TabIndex = 4;
+            this.tabPage2.Text = "Añadir";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(434, 43);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Agregar Administrador";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Contraseña";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(191, 102);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(139, 25);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(191, 161);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(139, 25);
+            this.txtUsername.TabIndex = 7;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(191, 220);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(139, 25);
+            this.txtPassword.TabIndex = 8;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(49, 311);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(109, 41);
+            this.btnAgregar.TabIndex = 9;
+            this.btnAgregar.Text = "Añadir";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(221, 311);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(109, 41);
+            this.btnSalir.TabIndex = 10;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(218, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 41);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Salir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(46, 318);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(109, 41);
+            this.btnEditar.TabIndex = 18;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // txtKilometraje
+            // 
+            this.txtKilometraje.Location = new System.Drawing.Point(152, 161);
+            this.txtKilometraje.Name = "txtKilometraje";
+            this.txtKilometraje.Size = new System.Drawing.Size(139, 25);
+            this.txtKilometraje.TabIndex = 17;
+            // 
+            // txtMotor
+            // 
+            this.txtMotor.Location = new System.Drawing.Point(152, 109);
+            this.txtMotor.Name = "txtMotor";
+            this.txtMotor.Size = new System.Drawing.Size(139, 25);
+            this.txtMotor.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Kilometraje";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Motor";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 59);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "ID de vehículo";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Black;
+            this.label8.Font = new System.Drawing.Font("Arial Rounded MT Bold", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Transparent;
+            this.label8.Location = new System.Drawing.Point(57, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(287, 43);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Editar vehículo";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(7, 216);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 17);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Color";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(7, 258);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(97, 17);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Transmisión";
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(152, 213);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(139, 25);
+            this.txtColor.TabIndex = 22;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(359, 88);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(185, 25);
+            this.txtDescripcion.TabIndex = 25;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(356, 59);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(96, 17);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "Descripción";
+            // 
+            // cbIdVehiculo
+            // 
+            this.cbIdVehiculo.FormattingEnabled = true;
+            this.cbIdVehiculo.Location = new System.Drawing.Point(152, 56);
+            this.cbIdVehiculo.Name = "cbIdVehiculo";
+            this.cbIdVehiculo.Size = new System.Drawing.Size(139, 25);
+            this.cbIdVehiculo.TabIndex = 26;
+            // 
+            // cbTransmision
+            // 
+            this.cbTransmision.FormattingEnabled = true;
+            this.cbTransmision.Location = new System.Drawing.Point(152, 255);
+            this.cbTransmision.Name = "cbTransmision";
+            this.cbTransmision.Size = new System.Drawing.Size(139, 25);
+            this.cbTransmision.TabIndex = 27;
+            // 
             // Administrador
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(186)))), ((int)(((byte)(83)))));
-            this.ClientSize = new System.Drawing.Size(455, 450);
+            this.ClientSize = new System.Drawing.Size(584, 452);
             this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Administrador";
@@ -122,6 +404,10 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage3.ResumeLayout(false);
             this.tabPage3.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -134,5 +420,31 @@
         private System.Windows.Forms.Button btnEliminar;
         private System.Windows.Forms.Button btnSalirB;
         private System.Windows.Forms.Button btnEliminarCarro;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.TextBox txtKilometraje;
+        private System.Windows.Forms.TextBox txtMotor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.ComboBox cbTransmision;
+        private System.Windows.Forms.ComboBox cbIdVehiculo;
     }
 }
--- Distribuidora de carros/Administrador.cs
+++ Distribuidora de carros/Administrador.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace Distribuidora_de_caroos
         public Administrador()
         {
             InitializeComponent();
+            CargarIdsVehiculos();
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -53,5 +55,156 @@ namespace Distribuidora_de_caroos
             frmDltCar.ShowDialog();
 
         }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text.Trim();
+            string username = txtUsername.Text.Trim();
+            string contra = txtPassword.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(contra))
+            {
+                MessageBox.Show("Por favor, complete todos los campos.");
+                return;
+            }
+
+            try
+            {
+                ConexionBD conexion = new ConexionBD();
+                using (SqlConnection conn = conexion.ObtenerConexion())
+                {
+                    conn.Open();
+                    string query = @"INSERT INTO Usuarios (Nombre, Username, Contra, tipoUsuario)
+                             VALUES (@Nombre, @Username, @Contra, 'Administrador')";
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Nombre", nombre);
+                        cmd.Parameters.AddWithValue("@Username", username);
+                        cmd.Parameters.AddWithValue("@Contra", contra);
+
+                        int rows = cmd.ExecuteNonQuery();
+                        if (rows > 0)
+                        {
+                            MessageBox.Show("Administrador agregado correctamente.");
+                            txtNombre.Clear();
+                            txtUsername.Clear();
+                            txtPassword.Clear();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se pudo agregar el administrador.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al agregar administrador: " + ex.Message);
+            }
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Login frmLogin = new Login();
+            this.Hide();
+            frmLogin.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Login frmLogin = new Login();
+            this.Hide();
+            frmLogin.Show();
+        }
+        private void CargarIdsVehiculos()
+        {
+            cbIdVehiculo.Items.Clear();
+            try
+            {
+                ConexionBD conexion = new ConexionBD();
+                using (SqlConnection conn = conexion.ObtenerConexion())
+                {
+                    conn.Open();
+                    string query = "SELECT Id_Vehiculo FROM Vehiculo";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            cbIdVehiculo.Items.Add(reader["Id_Vehiculo"].ToString());
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar IDs: " + ex.Message);
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (cbIdVehiculo.SelectedItem == null)
+            {
+                MessageBox.Show("Seleccione un vehículo.");
+                return;
+            }
+
+            int idVehiculo = int.Parse(cbIdVehiculo.SelectedItem.ToString());
+            string motor = txtMotor.Text.Trim();
+            string transmision = cbTransmision.Text;
+            decimal kilometraje = 0;
+            if (!decimal.TryParse(txtKilometraje.Text.Trim(), out kilometraje))
+            {
+                MessageBox.Show("Kilometraje inválido.");
+                return;
+            }
+            string color = txtColor.Text.Trim();
+            string descripcion = txtDescripcion.Text.Trim();
+
+            try
+            {
+                ConexionBD conexion = new ConexionBD();
+                using (SqlConnection conn = conexion.ObtenerConexion())
+                {
+                    conn.Open();
+                    string query = @"UPDATE Detalle_Vehiculo SET 
+                             Motor = @Motor,
+                             Transmision = @Transmision,
+                             Kilometraje = @Kilometraje,
+                             Color = @Color,
+                             Descripcion = @Descripcion
+                             WHERE Id_Vehiculo = @IdVehiculo";
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Motor", motor);
+                        cmd.Parameters.AddWithValue("@Transmision", transmision);
+                        cmd.Parameters.AddWithValue("@Kilometraje", kilometraje);
+                        cmd.Parameters.AddWithValue("@Color", color);
+                        cmd.Parameters.AddWithValue("@Descripcion", descripcion);
+                        cmd.Parameters.AddWithValue("@IdVehiculo", idVehiculo);
+
+                        int filas = cmd.ExecuteNonQuery();
+                        if (filas > 0)
+                        {
+                            MessageBox.Show("Vehículo actualizado correctamente.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se pudo actualizar el vehículo.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al actualizar: " + ex.Message);
+            }
+        }
+
     }
 }
+
